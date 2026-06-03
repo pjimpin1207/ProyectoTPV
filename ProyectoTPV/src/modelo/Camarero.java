68ade4210b8ac615be6a42c34dfdd02b4356453c
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2026-06-03 19:19:34 +0200

    Clase Camarero y Clase Administrador

--- ProyectoTPV/src/modelo/Camarero.java
+++ ProyectoTPV/src/modelo/Camarero.java
@@ -1,4 +1,24 @@
 package modelo;
 
-public class Camarero {
+import excepciones.MesaException;
+
+public class Camarero extends Usuario {
+
+    public Camarero(int id, String nombre, String password) {
+        super(id, nombre, password);
+    }
+
+    public void seleccionarMesa(Mesa m) throws MesaException {
+        if (m.getEstado() == EstadoMesa.RESERVADA) {
+            throw new MesaException("La mesa número " + m.getNumero() + " está reservada.");
+        }
+        m.cambiarEstado(EstadoMesa.OCUPADA);
+    }
+
+    public Ticket crearTicket(Mesa m, int numeroTicket) throws MesaException {
+        if (m.getEstado() != EstadoMesa.OCUPADA) {
+            throw new MesaException("No se puede crear un ticket si la mesa no está ocupada.");
+        }
+        return new Ticket(numeroTicket);
+    }
 }
